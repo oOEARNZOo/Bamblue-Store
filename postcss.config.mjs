a4cae05ec0576a83a4381f964586b41bde7df0c5
oOEARNZOo <earn.hcg32@gmail.com>
2026-03-11 13:32:14 +0700

    a

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,8 @@
+// filepath: d:\Python save\Landing page 1\bamblue-store\postcss.config.mjs
 const config = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
 
